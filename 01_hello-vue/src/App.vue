4b6b30912891cce37f0ba4a596ef44d4af12b278
Antony Neyret <antony.neyret@gmail.com>
2024-04-15 11:39:59 +0200

    init git

---
+++ 01_hello-vue/src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+import InputTemperature from './components/InputTemperature.vue'
+import {computed, ref, watch} from 'vue'
+
+const tempK = ref(11);
+
+const tempC = computed({
+  get: () => tempK.value - 273.15,
+  set: (val) => {
+    tempK.value = val + 273.15
+  }
+})
+
+const tempF = computed({
+  get: () => tempK.value * 9/5 - 459.67,
+  set: (val) => {
+    tempK.value = (val + 459.67) * 5/9
+  }
+})
+
+</script>
+
+<template>
+  <h1>Temperature convertor</h1>
+
+  <div>
+    <input type="number" v-model="tempK">
+    <input type="number" v-model="tempC">
+    <input type="number" v-model="tempF">
+
+  </div>
+
+
+  <!-- <InputTemperature :value="tempK" unit="Kelvin"/>
+  <InputTemperature :value="tempC" unit="Celsius"/>
+  <InputTemperature :value="tempF" unit="Fahrenheit"/> -->
+</template>
+
+<style scoped>
+  div {
+    display: flex;
+    flex-direction: column;
+    gap:20px;
+    width: 200px;
+  }
+</style>
---
+++ 01_hello-vue/src/components/InputTemperature.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+    defineProps({
+        value: Number,
+        unit: String,
+    })
+</script>
+
+<template>
+  <div>
+    <label :for="unit">{{ unit }}</label>
+    <input type="number" :id="unit" :value="value" v-model="tempK">
+  </div>
+</template>
+
+<style scoped>
+div {
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  width: 200px;
+}
+</style>
